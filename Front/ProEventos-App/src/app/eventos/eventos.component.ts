fd87954ae893306de3274039a357c575aa8229d0
Breno <breno.ga.duque@gmail.com>
2021-08-24 23:54:12 -0300

    feito o filtro

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -4,21 +4,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-
-  public eventos : any = []; 
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
   widthImg = 150;
   marginImg = 2;
   isImgCollapsed = false;
 
+  private _filtroLista: string = '';
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+  public set filtroLista(v: string) {
+    this._filtroLista = v;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.dataEvento.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.lote.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.qtdPessoas.toString().indexOf(filtrarPor) !== -1
+    );
   }
 
   public isCollapsed() {
@@ -27,9 +48,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      Response => this.eventos = Response,
-      Error => console.log(Error)
+      (Response) => {
+        (this.eventos = Response), (this.eventosFiltrados = this.eventos);
+      },
+      (Error) => console.log(Error)
     );
   }
-
 }
